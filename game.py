d4b77e029072c6237298811ba95b4396399d2f12
techsidh <78267410+techsidh@users.noreply.github.com>
2021-02-03 15:04:11 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,24 @@
+class rockpapersecissor:
+    def __init__(self, data):
+        self.data = data
+        self.rock = None
+        self.paper = None
+        self.secissor = None
+
+def game(self):
+    if rock == paper is True:
+        print(paper=+1)
+    elif secissor == paper is True:
+        print(secissor=+1)
+    elif rock == secissor is True:
+        print(rock =+1)
+    else:
+        if (" "==rock or self.data==paper or self.data == secissor):
+            print("game is tia!!!")
+        return game()
+
+
+data=input("enter your choice")
+rps=rockpapersecissor(data)
+print(rps.game())
+#print(rockpapersecissor(data))
